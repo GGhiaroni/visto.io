c73b41af729de53e482125b7b56cc870aae93c1f
GGhiaroni <gghiaronitiziano@gmail.com>
2025-11-22 21:01:56 -0300

    criando o meu componente Card

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { cn } from "../lib/utils";
+
+//CARD PRINCIPAL
+const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "rounded-xl border bg-card text-card-foreground shadow-sm bg-white",
+      className
+    )}
+    {...props}
+  />
+));
+
+Card.displayName = "Card";
+
+//CABEÇALHO DO CARD
+const CardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    {...props}
+  />
+));
+
+CardHeader.displayName = "CardHeader";
+
+//O TÍTULO EM SI
+const CardTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h3
+    ref={ref}
+    className={cn(
+      "text-2xl font-semibold leading-none tracking-tight text-forte",
+      className
+    )}
+    {...props}
+  />
+));
+
+CardTitle.displayName = "CardTitle";
+
+//O CONTEÚDO PRINCIPAL
+const CardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+));
+
+CardContent.displayName = "CardContent";
+
+export { Card, CardContent, CardHeader, CardTitle };

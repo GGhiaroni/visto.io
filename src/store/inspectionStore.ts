46022d2cc45900d3b88b1aecc970cc556dc6a865
GGhiaroni <gghiaronitiziano@gmail.com>
2025-11-27 00:01:45 -0300

    criando uma funcao para adicionar quartos na inspectionStore

--- src/store/inspectionStore.ts
+++ src/store/inspectionStore.ts
@@ -1,12 +1,13 @@
 import { v4 as uuidv4 } from "uuid";
 import { create } from "zustand";
-import type { Inspection } from "../types/inspection";
+import type { Inspection, Room } from "../types/inspection";
 
 //minha store de vistorias
 interface InspectionStore {
   //pode ser nula, caso não tenha nenhuma Vistoria aberta
   currentInspection: Inspection | null;
   createInspection: (address: string, clientName: string) => string;
+  addRoom: (roomName: string) => void;
 }
 
 export const useInspectionStore = create<InspectionStore>((set) => ({
@@ -30,4 +31,23 @@ export const useInspectionStore = create<InspectionStore>((set) => ({
 
     return id;
   },
+
+  addRoom: (roomName) =>
+    set((state) => {
+      if (!state.currentInspection) return state;
+
+      const newRoom: Room = {
+        id: uuidv4(),
+        name: roomName,
+        isCompleted: false,
+        items: [], // aqui, vou começar vazio
+      };
+
+      return {
+        currentInspection: {
+          ...state.currentInspection,
+          rooms: [...state.currentInspection.rooms, newRoom],
+        },
+      };
+    }),
 }));

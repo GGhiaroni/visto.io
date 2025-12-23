91f2085c757ba66f9adf95a590a0449d6767103a
GGhiaroni <gghiaronitiziano@gmail.com>
2025-12-23 14:49:36 -0300

    implementando o persist na storage

--- src/store/inspectionStore.ts
+++ src/store/inspectionStore.ts
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 import type {
   Annotation,
   Inspection,
@@ -25,179 +26,187 @@ interface InspectionStore {
   addPhoto: (roomId: string, itemId: string, photoUrl: string) => void;
 }
 
-export const useInspectionStore = create<InspectionStore>((set) => ({
-  currentInspection: null,
+export const useInspectionStore = create<InspectionStore>()(
+  persist(
+    (set) => ({
+      currentInspection: null,
 
-  //criando o objeto vistoria
-  createInspection: (address, clientName) => {
-    const id = uuidv4();
-    const newInspection: Inspection = {
-      id,
-      clientName,
-      propertyAddress: address,
-      date: Date.now(),
-      status: "draft", //começa como rascunho
-      rooms: [], //começa sem cômodos
-    };
-
-    set({ currentInspection: newInspection });
-
-    console.log("Nova vistoria criada na store:", newInspection);
-
-    return id;
-  },
-
-  addRoom: (roomName) =>
-    set((state) => {
-      if (!state.currentInspection) return state;
-
-      const newRoom: Room = {
-        id: uuidv4(),
-        name: roomName,
-        isCompleted: false,
-        items: [], // aqui, vou começar vazio
-      };
-
-      return {
-        currentInspection: {
-          ...state.currentInspection,
-          rooms: [...state.currentInspection.rooms, newRoom],
-        },
-      };
-    }),
-
-  addItemInspection: (roomId, itemName) =>
-    set((state) => {
-      if (!state.currentInspection) return state;
-
-      const newInspectionItem: InspectionItem = {
-        id: uuidv4(),
-        name: itemName,
-        status: "pending",
-        annotations: [],
-        photos: [],
-      };
-
-      const updatedRooms = state.currentInspection.rooms.map((room) => {
-        if (room.id === roomId) {
-          return {
-            ...room,
-            items: [...room.items, newInspectionItem],
-          };
-        }
-        return room;
-      });
-
-      return {
-        currentInspection: {
-          ...state.currentInspection,
-          rooms: updatedRooms,
-        },
-      };
-    }),
-
-  updateItemStatus: (roomId, itemId, newStatus) => {
-    set((state) => {
-      if (!state.currentInspection) return state;
-
-      const updatedRooms = state.currentInspection.rooms.map((room) => {
-        if (room.id !== roomId) return room;
-
-        const updatedItems = room.items.map((item) => {
-          if (item.id !== itemId) return item;
-
-          return {
-            ...item,
-            status: newStatus,
-          };
-        });
-
-        return {
-          ...room,
-          items: updatedItems,
+      //criando o objeto vistoria
+      createInspection: (address, clientName) => {
+        const id = uuidv4();
+        const newInspection: Inspection = {
+          id,
+          clientName,
+          propertyAddress: address,
+          date: Date.now(),
+          status: "draft", //começa como rascunho
+          rooms: [], //começa sem cômodos
         };
-      });
 
-      return {
-        currentInspection: {
-          ...state.currentInspection,
-          rooms: updatedRooms,
-        },
-      };
-    });
-  },
+        set({ currentInspection: newInspection });
 
-  addAnnotation: (roomId, itemId, text) => {
-    set((state) => {
-      if (!state.currentInspection) return state;
+        console.log("Nova vistoria criada na store:", newInspection);
 
-      const updatedRooms = state.currentInspection.rooms.map((room) => {
-        if (room.id !== roomId) return room;
+        return id;
+      },
 
-        const updatedItem = room.items.map((item) => {
-          if (item.id !== itemId) return item;
+      addRoom: (roomName) =>
+        set((state) => {
+          if (!state.currentInspection) return state;
 
-          const newAnnotation: Annotation = {
+          const newRoom: Room = {
             id: uuidv4(),
-            text: text,
-            timestamp: Date.now(),
+            name: roomName,
+            isCompleted: false,
+            items: [], // aqui, vou começar vazio
           };
 
           return {
-            ...item,
-            annotations: [...item.annotations, newAnnotation],
+            currentInspection: {
+              ...state.currentInspection,
+              rooms: [...state.currentInspection.rooms, newRoom],
+            },
           };
-        });
+        }),
 
-        return {
-          ...room,
-          items: updatedItem,
-        };
-      });
+      addItemInspection: (roomId, itemName) =>
+        set((state) => {
+          if (!state.currentInspection) return state;
 
-      return {
-        currentInspection: {
-          ...state.currentInspection,
-          rooms: updatedRooms,
-        },
-      };
-    });
-  },
-
-  addPhoto: (roomId, itemId, photoUrl) => {
-    set((state) => {
-      if (!state.currentInspection) return state;
-
-      const updatedRoom = state.currentInspection.rooms.map((r) => {
-        if (r.id !== roomId) return r;
-
-        const updatedItem = r.items.map((i) => {
-          if (i.id !== itemId) return i;
-
-          const newPhoto: Photo = {
+          const newInspectionItem: InspectionItem = {
             id: uuidv4(),
-            url: photoUrl,
-            timestamp: Date.now(),
+            name: itemName,
+            status: "pending",
+            annotations: [],
+            photos: [],
           };
+
+          const updatedRooms = state.currentInspection.rooms.map((room) => {
+            if (room.id === roomId) {
+              return {
+                ...room,
+                items: [...room.items, newInspectionItem],
+              };
+            }
+            return room;
+          });
 
           return {
-            ...i,
-            photos: [...i.photos, newPhoto],
+            currentInspection: {
+              ...state.currentInspection,
+              rooms: updatedRooms,
+            },
+          };
+        }),
+
+      updateItemStatus: (roomId, itemId, newStatus) => {
+        set((state) => {
+          if (!state.currentInspection) return state;
+
+          const updatedRooms = state.currentInspection.rooms.map((room) => {
+            if (room.id !== roomId) return room;
+
+            const updatedItems = room.items.map((item) => {
+              if (item.id !== itemId) return item;
+
+              return {
+                ...item,
+                status: newStatus,
+              };
+            });
+
+            return {
+              ...room,
+              items: updatedItems,
+            };
+          });
+
+          return {
+            currentInspection: {
+              ...state.currentInspection,
+              rooms: updatedRooms,
+            },
           };
         });
+      },
 
-        return {
-          ...r,
-          items: updatedItem,
-        };
-      });
+      addAnnotation: (roomId, itemId, text) => {
+        set((state) => {
+          if (!state.currentInspection) return state;
 
-      return {
-        currentInspection: {
-          ...state.currentInspection,
-          rooms: updatedRoom,
-        },
-      };
-    });
-  },
-}));
+          const updatedRooms = state.currentInspection.rooms.map((room) => {
+            if (room.id !== roomId) return room;
+
+            const updatedItem = room.items.map((item) => {
+              if (item.id !== itemId) return item;
+
+              const newAnnotation: Annotation = {
+                id: uuidv4(),
+                text: text,
+                timestamp: Date.now(),
+              };
+
+              return {
+                ...item,
+                annotations: [...item.annotations, newAnnotation],
+              };
+            });
+
+            return {
+              ...room,
+              items: updatedItem,
+            };
+          });
+
+          return {
+            currentInspection: {
+              ...state.currentInspection,
+              rooms: updatedRooms,
+            },
+          };
+        });
+      },
+
+      addPhoto: (roomId, itemId, photoUrl) => {
+        set((state) => {
+          if (!state.currentInspection) return state;
+
+          const updatedRoom = state.currentInspection.rooms.map((r) => {
+            if (r.id !== roomId) return r;
+
+            const updatedItem = r.items.map((i) => {
+              if (i.id !== itemId) return i;
+
+              const newPhoto: Photo = {
+                id: uuidv4(),
+                url: photoUrl,
+                timestamp: Date.now(),
+              };
+
+              return {
+                ...i,
+                photos: [...i.photos, newPhoto],
+              };
+            });
+
+            return {
+              ...r,
+              items: updatedItem,
+            };
+          });
+
+          return {
+            currentInspection: {
+              ...state.currentInspection,
+              rooms: updatedRoom,
+            },
+          };
+        });
+      },
+    }),
+    {
+      name: "inspection-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
